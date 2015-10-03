c8aea1b5645a8adafee3c7cc7219bc0baa830115
Vannarith Ek <vannarithek@Vannariths-MacBook-Pro.local>
2015-10-02 23:53:22 -0500

    update package.json, format code for readability

--- app/index.js
+++ app/index.js
@@ -6,59 +6,65 @@ var yeoman = require('yeoman-generator');
 var _s = require('underscore.string');
 
 module.exports = yeoman.generators.Base.extend({
-	init: function () {
-		var cb = this.async();
+    init: function() {
+        var cb = this.async();
 
-		this.prompt([{
-			name: 'appName',
-			message: 'What do you want to name your app?',
-			default: this.appname.replace(/\s/g, '-'),
-			filter: function (val) {
-				return _s.slugify(val);
-			}
-		}, {
-			name: 'githubUsername',
-			message: 'What is your GitHub username?',
-			store: true,
-			validate: function (val) {
-				return val.length > 0 ? true : 'You have to provide a username';
-			}
-		}, {
-			name: 'website',
-			message: 'What is the URL of your website?',
-			store: true,
-			validate: function (val) {
-				return val.length > 0 ? true : 'You have to provide a website URL';
-			},
-			filter: function (val) {
-				return normalizeUrl(val);
-			}
-		}], function (props) {
-			var tpl = {
-				appName: props.appName,
-				classifiedAppName: _s.classify(props.appName),
-				githubUsername: props.githubUsername,
-				name: this.user.git.name(),
-				email: this.user.git.email(),
-				website: props.website,
-				humanizedWebsite: humanizeUrl(props.website),
-				superb: superb()
-			};
+        this.prompt([
+	        {
+	            name: 'appName',
+	            message: 'What do you want to name your app?',
+	            default: this.appname.replace(/\s/g, '-'),
+	            filter: function(val) {
+	                return _s.slugify(val);
+	            }
+	        },
+	        {
+	            name: 'githubUsername',
+	            message: 'What is your GitHub username?',
+	            store: true,
+	            validate: function(val) {
+	                return val.length > 0 ? true : 'You have to provide a username';
+	            }
+	        },
+	        {
+	            name: 'website',
+	            message: 'What is the URL of your website?',
+	            store: true,
+	            validate: function(val) {
+	                return val.length > 0 ? true : 'You have to provide a website URL';
+	            },
+	            filter: function(val) {
+	                return normalizeUrl(val);
+	            }
+	        }
+        ], function(props) {
+            var tpl = {
+                appName: props.appName,
+                classifiedAppName: _s.classify(props.appName),
+                githubUsername: props.githubUsername,
+                name: this.user.git.name(),
+                email: this.user.git.email(),
+                website: props.website,
+                humanizedWebsite: humanizeUrl(props.website),
+                superb: superb()
+            };
 
-			var mv = function (from, to) {
-				this.fs.move(this.destinationPath(from), this.destinationPath(to));
-			}.bind(this);
+            var mv = function(from, to) {
+                this.fs.move(this.destinationPath(from), this.destinationPath(to));
+            }.bind(this);
 
-			this.fs.copyTpl(this.templatePath() + '/**', this.destinationPath(), tpl);
-			mv('editorconfig', '.editorconfig');
-			mv('gitattributes', '.gitattributes');
-			mv('gitignore', '.gitignore');
-			mv('_package.json', 'package.json');
+            this.fs.copyTpl(this.templatePath() + '/**', this.destinationPath(), tpl);
+            mv('editorconfig', '.editorconfig');
+            mv('gitattributes', '.gitattributes');
+            mv('gitignore', '.gitignore');
+            mv('_package.json', 'package.json');
 
-			cb();
-		}.bind(this));
-	},
-	install: function () {
-		this.installDependencies({bower: false});
-	}
+            cb();
+        }.bind(this));
+    },
+    install: function() {
+        this.installDependencies({
+            bower: false
+        });
+    }
 });
